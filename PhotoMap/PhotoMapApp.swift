a648967da7ae7c1b7e5d513e96de7f778ea39be3
Henny2 <henrieke.baunack@web.de>
2024-01-21 13:35:59 -0800

    Initial Commit

---
+++ PhotoMap/PhotoMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMapApp.swift
+//  PhotoMap
+//
+//  Created by Henrieke Baunack on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
